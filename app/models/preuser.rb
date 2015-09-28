edc71b2ab9a2f6de962cc654a295294e670d69a6
dnordby <nordbydaniel@gmail.com>
2015-09-28 14:44:20 -0400

    bug

--- app/models/preuser.rb
+++ app/models/preuser.rb
@@ -4,7 +4,7 @@ class Preuser < ActiveRecord::Base
     validates :referral_code, :uniqueness => true
 
     before_create :create_referral_code
-    before_save { self.email.downcase! }
+    before_create { self.email.downcase! }
 
     REFERRAL_STEPS = [
         {
