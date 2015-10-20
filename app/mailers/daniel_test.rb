131d4d649a28ed6f9a7efc5782b0d602f223fcbb
dnordby <nordbydaniel@gmail.com>
2015-10-20 11:24:21 -0400

    update

--- app/mailers/daniel_test.rb
+++ app/mailers/daniel_test.rb
@@ -1,18 +1,22 @@
 class DanielTest < ApplicationMailer
 
 	def fake_email
-		mail to: 'nordbydaniel@gmail.com', from: 'founders@tulipclean.com', subject: 'THIS IS A TEST'
+		mail to: 'nordbydaniel@gmail.com', from: 'sanjiv@tulipclean.com', subject: 'THIS IS A TEST'
 	end
 
 	def five_email
+		mail to: 'nordbydaniel@gmail.com', subject: 'TEST 5'
 	end
 
 	def ten_email
+		mail to: 'nordbydaniel@gmail.com', subject: 'TEST 10'
 	end
 
 	def twentyfive_email
+		mail to: 'nordbydaniel@gmail.com', subject: 'TEST 25'
 	end
 
 	def fifty_email
+		mail to: 'nordbydaniel@gmail.com', subject: 'TEST 50'
 	end
 end
